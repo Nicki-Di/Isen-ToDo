133e71e7312b9edeb3ebeb563e62e658f0c91a2b
jordangatsby <6kourosh.sh6@gmail.com>
2021-11-13 17:05:02 +0330

    Simple RadioButtons added.

--- app/src/main/java/com/example/isen_todo/MainActivity.kt
+++ app/src/main/java/com/example/isen_todo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.isen_todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aut.isen_todo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
